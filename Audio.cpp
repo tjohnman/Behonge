6778a4c744a2f95f6cb588166069031c55fd893b
tjohnman <ds_joan@yahoo.es>
2014-09-24 17:50:02 +0200

    Forgot to add files in previous commit.

---
+++ Audio.cpp
@@ -0,0 +1,53 @@
+#include "Audio.h"
+
+std::map<std::string, sf::SoundBuffer *> Audio::_s_Buffers;
+sf::Sound Audio::_s_Channels[256];
+int Audio::_s_ChannelIndex;
+
+bool Audio::Load(std::string path, std::string name)
+{
+	std::map<std::string, sf::SoundBuffer *>::iterator it;
+	it = _s_Buffers.find(name);
+	if(it == _s_Buffers.end())
+	{
+		sf::SoundBuffer * buffer = new sf::SoundBuffer();
+
+		if(buffer->loadFromFile(path))
+		{
+			_s_Buffers[name] = buffer;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	return true;
+}
+
+sf::Sound * Audio::Play(std::string name)
+{
+	std::map<std::string, sf::SoundBuffer *>::iterator it;
+	it = _s_Buffers.find(name);
+	if(it == _s_Buffers.end())
+	{
+		return 0;
+	}
+
+	sf::Sound * sound = &_s_Channels[_s_ChannelIndex++%256];
+
+	sound->stop();
+	sound->setBuffer(*it->second);
+	sound->play();
+
+	return sound;
+}
+
+void Audio::Cleanup()
+{
+	std::map<std::string, sf::SoundBuffer *>::iterator it;
+	for(it=_s_Buffers.begin(); it!=_s_Buffers.end(); ++it)
+	{
+		delete it->second;
+	}
+	_s_Buffers.clear();
+}
---
+++ Audio.h
@@ -0,0 +1,51 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include "includes.h"
+
+/*
+**
+**	This is a very simple static class used for
+**  loading and playing audio (not music).
+**
+**	For music sf::Music should be used.
+**	I don't think that needs any abstraction.
+**
+*/
+
+class Audio
+{
+public:
+	/*
+		Preload an audio clip for playing it later.
+		Assign it a name to use with the Play function.
+
+		If a file with the same name has already been loaded this
+		does nothing.
+
+		Returns true both if the file was loaded and
+		if it was already loaded in a previous call.
+	*/
+	static bool Load(std::string path, std::string name);
+
+	/*
+		Plays an audio clip.
+
+		Returns a pointer to the sf::Sound
+		for controlling playback or NULL if
+		no clip was loaded with that name.
+	*/
+	static sf::Sound * Play(std::string name);
+
+	/*
+		Unload everything
+	*/
+	static void Cleanup();
+
+private:
+	static std::map<std::string, sf::SoundBuffer *> _s_Buffers;
+	static sf::Sound _s_Channels[256];
+	static int _s_ChannelIndex;
+};
+
+#endif // AUDIO_H
